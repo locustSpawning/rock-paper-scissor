42f14ae7e9857b7fcbc1d1d497f1ea672609ff1e
chels <chelsleonhardt@gmail.com>
2023-03-22 20:29:38 -0400

    Finalized js - added counter for best out of 5 - returns a winner of the round - cleaned up code

--- game.js
+++ game.js
@@ -1,27 +1,67 @@
-
 let options = ['rock', 'paper', 'scissors'];
-let randIndex = Math.floor(Math.random() * options.length);
-randOption = options[randIndex];
+let numPlayerWins = 0;
+let numCompWins = 0;
 
 function getComputerChoice(){
-    console.log('computer: ' + randOption);
-    return randOption;
+    let randIndex = Math.floor(Math.random() * options.length);
+    return options[randIndex];
 }
 
 
 function getPlayerChoice(string){
-    let choice = prompt("Rock, paper, or scissors? ");
-    return console.log('player choice: ' + choice.toLocaleLowerCase());
+    return prompt("Rock, paper, or scissors? ");
 }
-
 
 
 function playRound(computerSelection, playerSelection){
-    if (computerSelection == 'rock' & playerSelection == 'scissors'){
-        return console.log('rock beats scissors!');
-    } 
+    if (computerSelection == playerSelection){
+        return 0;
+    }
+    if ((computerSelection == 'rock' & playerSelection == 'paper') |
+        (computerSelection == 'paper' & playerSelection == 'scissors') |
+        (computerSelection == 'scissors' & playerSelection == 'rock')){
+        numPlayerWins +=1;
+        return 2;
+    }
+    numCompWins += 1;
+    return 1;
 }
 
-let computerSelection = getComputerChoice()
-let playerSelection = getPlayerChoice()
-console.log(playRound(playerSelection, computerSelection));
+function printResult (result, computerSelection, playerSelection) {
+    switch (result){
+        case 0:  // tie
+            console.log('Tie! Play again!');
+            break;
+        case 1:  // computer wins
+            console.log('Computer wins! ' + computerSelection + ' beats ' + playerSelection);
+            break;
+        case 2:  // player wins
+            console.log('Player wins! ' + playerSelection + ' beats ' + computerSelection);
+            break;
+    }
+}
+
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let computerSelection = getComputerChoice();
+        console.log('computer: ' + computerSelection);
+        let playerSelection = getPlayerChoice();
+        
+        console.log('player choice: ' + playerSelection);
+        result = playRound(computerSelection, playerSelection);
+        printResult(result, computerSelection, playerSelection);
+    }
+}
+
+function finalResults(){
+    if (numPlayerWins > numCompWins){
+        console.log('Player wins this round!')
+    }
+    else{
+        console.log('Computer wins this round!')
+    }
+}
+
+game();
+finalResults();
