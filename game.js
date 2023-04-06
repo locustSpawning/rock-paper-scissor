f914490f0a838eac37d47bd13338265ca89f6958
chels <chelsleonhardt@gmail.com>
2023-04-06 16:24:33 -0400

    Finalized JS Runs through round displays winner Prints winner on screen w/score
    adjusted CSS
    -added glowing border
    - hover effect

--- game.js
+++ game.js
@@ -2,14 +2,24 @@ let options = ['rock', 'paper', 'scissors'];
 let numPlayerWins = 0;
 let numCompWins = 0;
 
+
 function getComputerChoice(){
     let randIndex = Math.floor(Math.random() * options.length);
     return options[randIndex];
 }
 
 
+
 function getPlayerChoice(string){
-    return prompt("Rock, paper, or scissors? ");
+    if (string === 'rock'){
+        playerSelection = 'rock'
+    }
+    else if (string === 'paper'){
+        playerSelection = 'paper'
+    }
+    else if (string === 'scissors'){
+        playerSelection = 'scissors'
+    }
 }
 
 
@@ -28,40 +38,97 @@ function playRound(computerSelection, playerSelection){
 }
 
 function printResult (result, computerSelection, playerSelection) {
+    let score = "<br>(" +"You: " + numPlayerWins + " - " + "Comp: " + numCompWins + ")"
     switch (result){
         case 0:  // tie
-            console.log('Tie! Play again!');
+            document.getElementById('print-content').innerHTML ='Tie! Play again!' +  score;
+            document.getElementById('print-content').style = 'color: #bec4cf;'
             break;
         case 1:  // computer wins
-            console.log('Computer wins! ' + computerSelection + ' beats ' + playerSelection);
+            document.getElementById('print-content').innerHTML ='Computer wins! ' + computerSelection + ' beats ' + playerSelection + score;
+            document.getElementById('print-content').style = 'color: #c98e3a;'
             break;
         case 2:  // player wins
-            console.log('Player wins! ' + playerSelection + ' beats ' + computerSelection);
+            document.getElementById('print-content').innerHTML = 'You win! ' + playerSelection + ' beats ' + computerSelection + score;
+            document.getElementById('print-content').style = 'color: #738eba;'
             break;
     }
 }
 
+function ComputerImage(computerSelection){
+    document.getElementById('computer-move').src = 'images/question.png';
+    document.getElementById('comp-text').innerHTML = " . . . ";
+    let myImage = 'images/rock.jpg'
+    if (computerSelection == 'paper'){
+        myImage = 'images/paper.jpg';
+    }
+    else if(computerSelection == 'scissors') {
+        myImage = 'images/scissors.jpg';
+    }
+    window.setTimeout(()=>{
+        document.getElementById('computer-move').src = myImage;
+        document.getElementById('comp-text').innerHTML =(computerSelection);
+    }, 500)
+}
 
 function game(){
-    for (let i = 0; i < 5; i++){
-        let computerSelection = getComputerChoice();
-        console.log('computer: ' + computerSelection);
-        let playerSelection = getPlayerChoice();
-        
-        console.log('player choice: ' + playerSelection);
-        result = playRound(computerSelection, playerSelection);
-        printResult(result, computerSelection, playerSelection);
+    let computerSelection = getComputerChoice();
+    ComputerImage(computerSelection);
+    
+    console.log('player choice: ' + playerSelection);
+    result = playRound(computerSelection, playerSelection);
+    printResult(result, computerSelection, playerSelection);
+
+    if (numPlayerWins + numCompWins >= 5){
+        finalResults()
     }
+}
+
+
+playing_button_ids = [
+    'rock_button',
+    'paper_button',
+    'scissors_button'
+]
+
+
+end_button_ids = [
+    "end-play-button",
+    "end-quit-button"
+]
+
+function moveButtons(some_div, elements){
+    elements.forEach((elename)=>{
+        element = document.getElementById(elename);
+        element.remove();
+        some_div.appendChild(element);
+    })
+    
 }
 
 function finalResults(){
+
+    let hidden_div = document.getElementById('hidden_things');
+    let show_div = document.getElementById('player_buttons');
+    moveButtons(hidden_div, playing_button_ids);
+    moveButtons(show_div, end_button_ids);
+
+
+
     if (numPlayerWins > numCompWins){
-        console.log('Player wins this round!')
+        document.getElementById('print-content').innerHTML = ('You win this round!')
+        document.getElementById('print-content').style = 'color: #738eba;'
     }
     else{
-        console.log('Computer wins this round!')
+        document.getElementById('print-content').innerHTML = ('Computer wins this round!')
+        document.getElementById('print-content').style = 'color: #c98e3a;'
     }
+    
+    
 }
 
-game();
-finalResults();
+function playerButtonClicked(string){
+    getPlayerChoice(string);
+    game();
+
+}
